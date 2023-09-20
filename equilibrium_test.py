b1aca82e6bf183dfdb7ba7c5ddb0c0b012b747c7
[Martin Brolly] <[martinbrolly@live.co.uk]>
2023-09-20 09:56:13 +0100

    Remove obselete files.

--- equilibrium_test.py
+++
@@ -1,9 +0,0 @@
-from model import Model
-import inits
-
-m = Model(nx=1024, Tend=500., twrite=40, dt=2.5e-4,
-          data_dir='data/equilibrium_test/',
-          k_f=64, f_a=1e10, lsf_a=1., dealias=True)
-
-m.zk = inits.zero(m)
-m.run()
